fa839dbbf08a0b6aa40c5ed0022f38953b622836
Tassad <Tassadaritze@gmail.com>
2021-11-24 23:52:44 +0300

    Move dotenv configurator

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
-import "./env.js";
-// import { Client, Intents } from "discord.js";
+import "../env/env.js";
+import { Client, Intents } from "discord.js";
 
-// const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once("ready", () => console.log("Ready!"));
+
+client.login(process.env.DISCORD_TOKEN);
