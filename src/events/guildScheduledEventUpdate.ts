5efe7734189439a783b14cd3db59bfc1c836c5b1
Tassad <Tassadaritze@gmail.com>
2021-12-26 11:44:45 +0300

    Fix the bot treating events starting as their start time changing again

--- src/events/guildScheduledEventUpdate.ts
+++ src/events/guildScheduledEventUpdate.ts
@@ -6,8 +6,7 @@ export default {
     async execute(oldGuildScheduledEvent: GuildScheduledEvent, newGuildScheduledEvent: GuildScheduledEvent) {
         const client = oldGuildScheduledEvent.client as ClientPlus;
         if (
-            oldGuildScheduledEvent.scheduledStartAt &&
-            newGuildScheduledEvent.scheduledStartAt &&
+            newGuildScheduledEvent.status === "SCHEDULED" &&
             oldGuildScheduledEvent.scheduledStartAt !== newGuildScheduledEvent.scheduledStartAt
         ) {
             client.eventReportChannel?.send(`Event **${newGuildScheduledEvent.name}**, previously scheduled for ${oldGuildScheduledEvent.scheduledStartAt.toUTCString()} will now start at **${newGuildScheduledEvent.scheduledStartAt.toUTCString()}**.`);
