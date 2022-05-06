bdc5b6c2e16fb94944e14eeed1cd8f0a8b42a822
Tassad <Tassadaritze@gmail.com>
2022-05-06 18:06:59 +0300

    Implement more bits of tic-tac-toe module

--- src/classes/TicTacToe.ts
+++ src/classes/TicTacToe.ts
@@ -1,26 +1,56 @@
-import { User } from "discord.js";
+import {
+    Message,
+    MessageActionRow,
+    MessageButton,
+    MessageOptions,
+    TextChannel,
+    User
+} from "discord.js";
 
-type TicTacToeSpace = "X" | "O" | " ";
-interface Players {
-    x: User,
-    o: User
-}
+type TicTacToeSpace = "x" | "o" | null;
 
-// Keeps track of board state
+// Manages board state
 class TicTacToe {
     #board: TicTacToeSpace[][] = [
-        [" ", " ", " "],
-        [" ", " ", " "],
-        [" ", " ", " "]
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
     ];
     #xTurn = true;
-    #players: Players;
+    #players: User[];
+    #channel: TextChannel;
+    #message: Message | undefined;
 
-    constructor(players: Players) {
+    constructor(players: User[], channel: TextChannel) {
         this.#players = players;
+        this.#channel = channel;
+        this.#update();
     }
 
-
+    #update = (): void => {
+        const emoji = {
+            x: "❌",
+            o: "⭕"
+        }
+        const message: MessageOptions = { components: [] };
+        for (let i = 0; i < this.#board.length; ++i) {    // iterate through rows
+            const buttons = new MessageActionRow();
+            for (let j = 0; j < this.#board[i].length; ++j) {     // iterate through spaces
+                const space = this.#board[i][j];
+                const button = new MessageButton()
+                    .setStyle("SECONDARY")
+                    .setCustomId(`[${i},${j}]`);
+                if (space)
+                    button.setEmoji(emoji[space]);
+                else
+                    button.setLabel(" ");
+                buttons.addComponents(button);
+            }
+            message.components?.push(buttons);
+        }
+        this.#channel.send(message)
+            .then(message => this.#message = message);
+    }
 }
 
 export default TicTacToe;
--- src/commands/tictactoe.ts
+++ src/commands/tictactoe.ts
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder, SlashCommandUserOption } from "@discordjs/builders";
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, TextChannel } from "discord.js";
+import TicTacToe from "../classes/TicTacToe.js";
 
 export default {
     data: new SlashCommandBuilder()
@@ -10,6 +11,16 @@ export default {
                 .setName("opponent")
                 .setDescription("Pick your opponent (omit to let someone join later)")),
     async execute(interaction: CommandInteraction) {
-        // Put code here :^)
+        let tictactoe: TicTacToe;
+        const opponent = interaction.options.getUser("opponent");
+        if (interaction.channel instanceof TextChannel) {
+            if (opponent)
+                tictactoe = new TicTacToe([interaction.user, opponent], interaction.channel);
+            else
+                tictactoe = new TicTacToe([interaction.user], interaction.channel);
+        } else {
+            console.error("[ERROR] interaction.channel is not an instance of TextChannel");
+        }
+        await interaction.reply("Match time!");
     }
 }
