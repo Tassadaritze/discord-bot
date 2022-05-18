471f1a7ec58f67100bc714479390af7eee0e34f0
Tassad <Tassadaritze@gmail.com>
2022-05-19 00:39:44 +0300

    Fix /danbooru command failing at times

--- src/commands/danbooru.ts
+++ src/commands/danbooru.ts
@@ -17,43 +17,31 @@ export default {
         if (typeof tag === "string")
             tag = tag.trim().replaceAll(" ", "_");
 
-        let timeout = false;
-        const interactionTimeout = setTimeout(() => {
-            timeout = true;
-            interaction.deferReply();
-        }, 2500);
+        await interaction.deferReply();
 
         const nsfw = interaction.channel instanceof TextChannel && interaction.channel.nsfw;
         const response = await fetch(`https://danbooru.donmai.us/posts/random.json?tags=${!nsfw ? "rating:safe" : ""}${tag ? `+${tag}` : ""}&login=${process.env.DANBOORU_USERNAME}&api_key=${process.env.DANBOORU_API_KEY}`);
 
-        clearTimeout(interactionTimeout);
-
         let data = await response.json();
         if (!isPostData(data)) {
-            await interaction.reply(`**${tag ? tag : "random"}:** _Danbooru has returned an error.${!nsfw ? " (Trying this again in an NSFW channel might help.)" : ""}_`);
+            await interaction.editReply(`**${tag ? tag : "random"}:** _Danbooru has returned an error.${!nsfw ? " (Trying this again in an NSFW channel might help.)" : ""}_`);
             return;
         }
 
         const image = data.file_size > 8 * 1024 * 1024 ? sanitize(data.large_file_url) : sanitize(data.file_url);    // 8 MiB
-        if (timeout)
-            await interaction.editReply({
-                content: image ? `**${tag ? tag : "random"}:**` : "_Couldn't download image from unsafe URL._",
-                files: image ? [image] : undefined
-            });
-        else
-            await interaction.reply({
-                content: image ? `**${tag ? tag : "random"}:**` : "_Couldn't download image from unsafe URL._",
-                files: image ? [image] : undefined
-            });
+        await interaction.editReply({
+            content: image ? `**${tag ? tag : "random"}:**` : "_Couldn't download image from unsafe URL._",
+            files: image ? [image] : undefined
+        });
     }
-}
+};
 
 const sanitize = (url: string): string | null => {
     const SAFE_URL_PATTERN = /^(?:(?:https?|mailto|ftp|tel|file):|[^&:/?#]*(?:[/?#]|$))/gi;
 
     if (url.match(SAFE_URL_PATTERN)) return url;
     else return null;
-}
+};
 
 type PostData = {
     file_size: number,
@@ -63,4 +51,4 @@ type PostData = {
 
 const isPostData = (object: any): object is PostData => {
     return !!(object.file_size && object.file_url && object.large_file_url);
-}
+};
